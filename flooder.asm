4c1a478b52ccb40322c6aa315d660d0a4cdd6000
Weeke <baileydunlap9@gmail.com>
2020-01-12 05:52:12 -0800

    Update flooder.asm

--- flooder.asm
+++ flooder.asm
@@ -78,7 +78,8 @@ _start:                                                                         
 
 
         ; Loop for var time
-        mov BYTE PTR cl, [time]
+        ;mov BYTE PTR cl, [time]
+        mov ecx, 10
         l1:                                                                     ; Body of Loop
            ; Send Crafted UDP packet till time var ends from loop
            mov edx, lenGetTest                                                  ; Message Length
